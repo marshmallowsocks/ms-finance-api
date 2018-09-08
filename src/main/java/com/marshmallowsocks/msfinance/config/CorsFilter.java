0e70fed663288b020fe9cef2b4171875e4ddfcd4
marshmallowsocks <srivathsan.jayaraman@gmail.com>
2018-09-07 20:06:18 -0700

    hmm 2

--- src/main/java/com/marshmallowsocks/msfinance/config/CorsFilter.java
+++ src/main/java/com/marshmallowsocks/msfinance/config/CorsFilter.java
@@ -1,11 +1,16 @@
 package com.marshmallowsocks.msfinance.config;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
     private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(CorsFilter.class);
