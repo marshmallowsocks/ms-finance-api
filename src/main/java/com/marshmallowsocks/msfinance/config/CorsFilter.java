cbc03266d6733579b67981c267b342842ff94068
marshmallowsocks <srivathsan.jayaraman@gmail.com>
2018-09-07 20:25:09 -0700

    hmm 3

--- src/main/java/com/marshmallowsocks/msfinance/config/CorsFilter.java
+++ src/main/java/com/marshmallowsocks/msfinance/config/CorsFilter.java
@@ -3,9 +3,11 @@ package com.marshmallowsocks.msfinance.config;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -23,13 +25,22 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
         LOGGER.info("Filtering CORS..");
+
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Max-Age", "180");
-        filterChain.doFilter(servletRequest, servletResponse);
+
+        if(request.getMethod().equals(HttpMethod.OPTIONS.name())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+        else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
     }
 
     @Override
