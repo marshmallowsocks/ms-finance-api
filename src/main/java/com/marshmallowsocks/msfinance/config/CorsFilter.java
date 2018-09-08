e99d97d4b3a98b8b96ff3c24e250070a4df341ca
marshmallowsocks <srivathsan.jayaraman@gmail.com>
2018-09-07 20:00:08 -0700

    Added logging

--- src/main/java/com/marshmallowsocks/msfinance/config/CorsFilter.java
+++ src/main/java/com/marshmallowsocks/msfinance/config/CorsFilter.java
@@ -1,11 +1,14 @@
 package com.marshmallowsocks.msfinance.config;
 
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CorsFilter implements Filter {
+
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(CorsFilter.class);
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -15,8 +18,7 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        HttpServletRequest request= (HttpServletRequest) servletRequest;
-
+        LOGGER.info("Filtering CORS..");
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "*");
