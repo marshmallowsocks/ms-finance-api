b4a2b88dc38c8c741605618bfa22ea8b26e56a5a
marshmallowsocks <srivathsan.jayaraman@gmail.com>
2018-09-07 19:28:59 -0700

    CORS config for dev

--- src/main/java/com/marshmallowsocks/msfinance/user/controller/PublicUsersController.java
+++ src/main/java/com/marshmallowsocks/msfinance/user/controller/PublicUsersController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/public/users")
-@CrossOrigin(origins = "http://localhost:3000")
 public class PublicUsersController {
 
     private final AuthenticationService authenticationService;
