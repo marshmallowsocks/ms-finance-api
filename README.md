c98c5a3ee88ea6b1178497a285208d2d495a041d
Vatsa <srivathsan.jayaraman@gmail.com>
2018-11-07 01:28:19 -0800

    Start increasing coverage

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # ms-finance-api
 [![Build Status](https://travis-ci.org/marshmallowsocks/ms-finance-api.svg?branch=master)](https://travis-ci.org/marshmallowsocks/ms-finance-api)
-This project contains the backend API for Marshmallowsocks Finance Cloud. 
+[![Coverage Status](https://coveralls.io/repos/github/marshmallowsocks/ms-finance-api/badge.svg?branch=master)](https://coveralls.io/github/marshmallowsocks/ms-finance-api?branch=master)
+
+------------------------------------------------------------------------
+This project contains the backend API for Marshmallowsocks Finance Cloud.
 Just run spring-boot!
--- src/test/java/com/marshmallowsocks/msfinance/auth/TokenAuthenticationServiceTest.java
+++ src/test/java/com/marshmallowsocks/msfinance/auth/TokenAuthenticationServiceTest.java
@@ -2,30 +2,29 @@ package com.marshmallowsocks.msfinance.auth;
 
 import com.marshmallowsocks.msfinance.auth.service.TokenAuthenticationService;
 import com.marshmallowsocks.msfinance.auth.token.JwtToken;
+import com.marshmallowsocks.msfinance.auth.token.JwtTokenManager;
 import com.marshmallowsocks.msfinance.auth.token.TokenService;
 import com.marshmallowsocks.msfinance.user.model.User;
 import com.marshmallowsocks.msfinance.user.service.UserService;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
@@ -43,17 +42,28 @@ public class TokenAuthenticationServiceTest {
     @Mock
     private User user;
 
+    @Spy
+    private JwtTokenManager jwtTokenManager;
+
     @InjectMocks
     private TokenAuthenticationService tokenAuthenticationService;
 
     @Before
     public void setup() {
+        Map<String, String> userAttributes = new HashMap<>();
+        userAttributes.put("username", "A user");
+
         when(jwtToken.getToken()).thenReturn("A valid token");
         when(jwtToken.getExpirationMillis()).thenReturn(1000L);
+        when(jwtTokenService.verify("A valid token"))
+                .thenReturn(userAttributes);
         when(jwtTokenService.expiring(any(Map.class)))
                 .thenReturn(jwtToken);
 
-//        when(user.getUsername()).thenReturn("A user");
+        doCallRealMethod().when(jwtTokenService)
+                .invalidate(any(JwtToken.class));
+
+        when(user.getUsername()).thenReturn("A user");
         when(user.getPassword()).thenReturn(BCrypt.hashpw("A password", BCrypt.gensalt()));
         when(mongoUserService.findByUserName("A user"))
                 .thenReturn(Optional.of(user));
@@ -66,10 +76,10 @@ public class TokenAuthenticationServiceTest {
         JwtToken token;
 
         // assert
-        Assert.assertTrue(tokenOptional.isPresent());
+        assertTrue(tokenOptional.isPresent());
         token = tokenOptional.get();
 
-        verify(mongoUserService, times(1)).findByUserName("A user");
+        verify(mongoUserService, times(1)).findByUserName(eq("A user"));
         assertEquals(token.getToken(), jwtToken.getToken());
         assertEquals(token.getExpirationMillis(), jwtToken.getExpirationMillis());
     }
@@ -81,5 +91,28 @@ public class TokenAuthenticationServiceTest {
 
         // assert
         assertEquals(tokenOptional, Optional.empty());
+    }
+
+    @Test
+    public void findUserByToken_userExists() {
+        // act
+        Optional<User> userOptional = tokenAuthenticationService.findByToken("A valid token");
+        User user;
+
+        // assert
+        assertTrue(userOptional.isPresent());
+        user = userOptional.get();
+        verify(jwtTokenService, times(1)).verify(eq("A valid token"));
+        assertEquals(user.getUsername(), "A user");
+        assertEquals(user.getPassword(), BCrypt.hashpw("A password", BCrypt.gensalt()));
+    }
+
+    @Test
+    public void invaliateToken_tokenFetchFails() {
+        // act
+        tokenAuthenticationService.logout("A valid token");
+
+        // assert
+        assertTrue(jwtTokenManager.isInvalid("A valid token"));
     }
 }
